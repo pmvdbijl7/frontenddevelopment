4637b91fa07495752a7358777f84cf6128d0af1b
pmvdbijl7 <47750625+pmvdbijl7@users.noreply.github.com>
2021-01-13 21:36:43 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,8 @@
-// JavaScript Document
+/* Code to open the nav menu */
+var menuBtn = document.querySelector("nav button");
+menuBtn.addEventListener("click", openMenu);
+
+function openMenu() {
+    var navigation = document.querySelector("nav ul");
+    navigation.classList.toggle("show-nav");
+}
